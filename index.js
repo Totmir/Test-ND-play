517de219ae0f3a510837f582395ac0d11a345380
Totmir <42805085+Totmir@users.noreply.github.com>
2019-11-07 08:13:16 +0300

    Added comments & renamed some items

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Server settings
 const HOST = "http://edu.nd.ru/tests/test1.json";
+// Proxy address to send crossorigin request
 const PROXY = "https://cors-anywhere.herokuapp.com/";
 
-// Object data template
+// Object data keys template
 const TABLE_HEADER_TRANSLATIONS = {
   id: "ID",
   fullname: "Имя",
@@ -19,29 +20,32 @@ const TABLE_HEADER_TRANSLATIONS = {
   workflow: "Распорядок работы"
 };
 
-// Rerender the table after getting the data from URL
+// Rerenders the table after getting the data from URL
 class TableRenderer {
   generateTableBody = dataSet => {
     const check = document.querySelector("tbody");
     const tableBody = document.createElement("tbody");
     const tableNode = document.querySelector("table");
+    // Remove table body before generation procces if table body exists
     if (check) {
       tableNode.removeChild(check);
     }
     tableNode.appendChild(tableBody);
+    // Creates row for each object
     dataSet.forEach(item => {
       const tableCells = Object.keys(item);
       const tableBodyRow = document.createElement("tr");
+      tableBody.appendChild(tableBodyRow);
+      // Creates table cell for each object key
       tableCells.forEach(key => {
         const td = document.createElement("td");
-        td.innerHTML = item[key];
+        td.textContent = item[key];
         tableBodyRow.appendChild(td);
       });
-      tableBody.appendChild(tableBodyRow);
     });
   };
 
-  // Renders columns with keys from HOST
+  // Creates columns with keys from HOST
   generateTableColumns = columnConfig => {
     const tableColumns = Object.keys(columnConfig);
     const tableNode = document.querySelector("table");
@@ -50,7 +54,7 @@ class TableRenderer {
 
     tableColumns.forEach(tableHead => {
       const th = document.createElement("th");
-      th.innerHTML = TABLE_HEADER_TRANSLATIONS[tableHead];
+      th.textContent = TABLE_HEADER_TRANSLATIONS[tableHead];
       tableHeadRow.appendChild(th);
     });
 
@@ -66,7 +70,7 @@ function getData(url) {
     .then(result => result);
 }
 
-// Input area
+// Input area matching
 const searchInput = document.querySelector("input");
 const changeInputValue = e => {
   const clearButton = document.getElementById("clearButton");
@@ -92,7 +96,7 @@ const clearInput = () => {
   const searchInput = document.querySelector("input");
   if (searchInput.value === "") {
     return null;
-  }
+  } // if nothing is written nothing comeback
   searchInput.value = "";
 };
 
@@ -104,17 +108,18 @@ const checkInput = () => {
   const total = document.getElementById("total");
 
   if (searchInput.value !== "") {
-    const filtered = window.data.filter(item => {
-      const keys = Object.keys(item);
-      const temp = keys
+    const filtered = window.data.filter(teacher => {
+      const keys = Object.keys(teacher);
+      const searchResult = keys
         .map(
-          k =>
-          item[k].toLowerCase().search(searchInput.value.toLowerCase()) !== -1
-        )
-        .filter(item => item);
-      return temp.includes(true);
+          letter =>
+          teacher[letter].toLowerCase().search(searchInput.value.toLowerCase()) !== -1
+        ) // Generate new array only with letter-matching items
+      console.log(searchResult)
+      const s = searchResult.filter(item => item);
+      return s.includes(true);
     });
-    total.innerHTML = "Всего найдено: " + filtered.length;
+    total.textContent = "Всего найдено: " + filtered.length;
     return tableRenderer.generateTableBody(filtered);
   }
 };
